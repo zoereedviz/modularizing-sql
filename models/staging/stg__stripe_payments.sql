53f086a2055ac57aa3b9952b44781a1128646405
Zoe Reed <zoe.reed@theinformationlab.co.uk>
2025-02-21 15:24:00 +0000

    trial

--- models/staging/stg__stripe_payments.sql
+++ models/staging/stg__stripe_payments.sql
@@ -1,25 +1,17 @@
--- Plan is to build this one live
-
 with source as (
 
-    select * from {{ source('stripe', 'payment') }}
-
-),
-
-transformed as (
-
-    select
-
-        id as payment_id,
-        orderid as order_id,
-        paymentmethod as payment_method,
-        status as payment_status,
-        amount / 100 as payment_amount_$,
-        created as payment_date,
-        _batched_at
-
-    from source
+select * from {{source('stripe', 'payment')}}
 
 )
+transformed as (
+    id as payment_id,
+    orderid as order_id,
+    amount/100 as payment_amount_$,
+    created as payment_date,
+    paymentmethod as payment_method,
+    status as paymnet_status,
+    _batched_at
+
+ from source )
 
 select * from transformed
